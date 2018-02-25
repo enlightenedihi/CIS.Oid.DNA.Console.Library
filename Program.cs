6a87edf49df4750ec5cd774d6e2e7db705cfe477
enlightenedihi <enlightenihi@live.com>
2018-02-25 08:52:53 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS.Oid.DNA.Console.Library
+{
+   
+}
+
+namespace NCE
+{
+    public interface IDNA               // 
+    {
+        int DNA();
+    }
+
+    public class Program : IDNA
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int DNA()
+        {
+            return 234;
+        }
+    }
+}
